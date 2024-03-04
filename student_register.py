ea29451359a94a8c90594667d413e492278a7f9c
HJH94 <harry.hall1994@gmail.com>
2024-03-04 20:03:35 +0000

    Create student_register.py

---
+++ student_register.py
@@ -0,0 +1,16 @@
+# user_file = open(r'C:\Users\harry\Documents\Python Code Camp\Tasks' 'reg_form.txt', 'w') 
+
+user_file = open('reg_form.txt', 'w') 
+student_count = int(input("Please enter the number of students on the register: \n"))
+
+print(f"You have entered {student_count} students.") 
+
+for student_reg in range(student_count): 
+    student_id = input("Please enter the student's ID number: \n")
+    user_file.write('......................................................................... \n')
+    user_file.write(student_id + '\n') 
+
+user_file.write('...........................END OF REGISTER............................... \n')
+
+user_file.close() 
+
